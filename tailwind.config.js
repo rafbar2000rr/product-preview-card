74b679f806817e495bfe91bfdb367ce4d5c862bd
Rafael Ibarra <rafbar2000rr@gmail.com>
2024-10-22 19:55:26 -0500

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {colors:
+      {
+        darkcyan:'hsl(158, 36%, 37%)',
+        cream:'hsl(30, 38%, 92%)',
+        verydarkblue: 'hsl(212, 21%, 14%)',
+        darkgrayishblue: 'hsl(228, 12%, 48%)',
+        white: 'hsl(0, 0%, 100%)'
+      },
+    
+    fontFamily: {
+     
+      montserrat: ['Montserrat', 'sans-serif'],
+      fraunces: ['Fraunces', 'serif'],
+    },
+    screens: {
+      'custom': '376px', // Define custom breakpoint for 375px
+    },
+  },
+},
+  plugins: [],
+}
+
